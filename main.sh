c66c47377d7aab00c4c8677eb11357437cdc3221
semih kaynar <semih.kaynar55@gmail.com>
2026-06-01 17:57:23 +0300

    finish

--- main.sh
+++ main.sh
@@ -5,27 +5,36 @@
 # Sertifika 2: https://credsverse.com/credentials/5c6c792e-253d-4ebd-b4e3-5e8fb97e373a
 
 LOGFILE="report.log"
+
 echo "Başlangıç Tarihi: $(date -u +"%Y-%m-%dT%H:%M:%SZ")" > "$LOGFILE"
+
 echo "" >> "$LOGFILE"
 echo "işlemci" >> "$LOGFILE"
-wmic cpu get Name >> "$LOGFILE" 2>/dev/null
+lscpu | grep "Model name" >> "$LOGFILE" 2>/dev/null
+
 echo "" >> "$LOGFILE"
 echo "ram" >> "$LOGFILE"
-wmic computersystem get TotalPhysicalMemory >> "$LOGFILE" 2>/dev/null
+free -h >> "$LOGFILE" 2>/dev/null
+
 echo "" >> "$LOGFILE"
 echo "anakart" >> "$LOGFILE"
-wmic baseboard get Product,SerialNumber >> "$LOGFILE" 2>/dev/null
+cat /sys/class/dmi/id/board_name 2>/dev/null >> "$LOGFILE"
+cat /sys/class/dmi/id/board_vendor 2>/dev/null >> "$LOGFILE"
+
 echo "" >> "$LOGFILE"
 echo "disk uuid" >> "$LOGFILE"
-wmic csproduct get UUID >> "$LOGFILE" 2>/dev/null
+blkid >> "$LOGFILE" 2>/dev/null
+
 echo "" >> "$LOGFILE"
 echo "mac adresleri" >> "$LOGFILE"
-getmac >> "$LOGFILE" 2>/dev/null
+ip link | grep link/ether >> "$LOGFILE" 2>/dev/null
+
 echo ""
-# Kullanicidan gizli sifre alma (Sen MYO+202 gireceksin)
 read -s -p "Parola (MYO+202): " PAROLA
 echo ""
+
 echo "$PAROLA" | gpg --symmetric --batch --yes --passphrase-fd 0 --cipher-algo AES256 -o report.log.gpg "$LOGFILE"
+
 if [ $? -eq 0 ]
 then
     echo ""
